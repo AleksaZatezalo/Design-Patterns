a72660ac914df676c58ff5cdb4faeb3666f91d8b
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-12-10 18:34:28 -0500

    Folder Structure Created.

---
+++ Structural Patterns/adapter.java
@@ -0,0 +1,16 @@
+/**
+* Converts the interface of one class into another.
+* The class is created so we only have to overright a set number of functions.
+* The Adaptee implements a specific request.
+* @author Aleksa Zatezalo
+* @version 1.0
+* @since   2022-11-30 
+*/
+
+
+class Adapter {
+    public void core(){
+        return;
+    }
+
+}
